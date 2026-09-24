13b86a8fd18c6cc82b00ec7cb21e98405aaaeaac
CarlosFedericoZalazar <carlos-zalazar@hotmail.com>
2026-09-24 11:00:44 +0200

    seguimos

---
+++ CODES/Tarea5ConstantesLiterales/src/Enunciado6.java
@@ -0,0 +1,27 @@
+/*
+6. Para cada corredor, muestra nombre, tiempo final y mejora respecto a su marca anterior:
+ Nombre      Tiempo    Mejora
+ Ana Pérez   42.35     +1.20
+ Luis Gómez  39.80     -0.45
+1º DAW | PROGRAMACIÓN
+
+• Nombre: ancho 16.
+• Tiempo: 2 decimales y ancho 7.
+• Mejora: ancho 6 y signo obligatorio.
+*/
+
+import java.util.Scanner;
+
+public class Enunciado6 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("NOMBRE CORREDOR: ");
+        String corredorUno = sc.nextLine();
+
+        System.out.println("TIEMPO: ");
+        float tiempoUno = sc.nextFloat();
+
+        
+    }
+}
